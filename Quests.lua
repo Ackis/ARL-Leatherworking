5be7133cb7bbf117d4420a53a0e709414e98c43b
Ressy <resa1983@hotmail.com>
2016-08-29 23:40:34 -0400

    Add quests

--- Quests.lua
+++ Quests.lua
@@ -38,6 +38,23 @@ function module:InitializeQuests()
 	AddQuest(1582,	Z.DARNASSUS,			60.5,	37.3,	"Alliance")
 	AddQuest(7493,	Z.ORGRIMMAR,			51.0,	76.5,	"Horde")
 	AddQuest(7497,	Z.STORMWIND_CITY,		67.2,	85.5,	"Alliance")
+	AddQuest(40176,	Z.DALARAN_BROKENISLES,		35.2,	29.2,	"Neutral")
+	AddQuest(40178,	Z.DALARAN_BROKENISLES,		35.4,	29.8,	"Neutral")
+	AddQuest(40179,	Z.DALARAN_BROKENISLES,		35.4,	29.8,	"Neutral")
+	AddQuest(40183,	Z.DALARAN_BROKENISLES,		35.2,	29.2,	"Neutral")
+	AddQuest(40185,	Z.HIGHMOUNTAIN,			0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(40186,	Z.HIGHMOUNTAIN,			0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(40191,	Z.HIGHMOUNTAIN,			0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(40192,	Z.HIGHMOUNTAIN,			0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(40198,	Z.DALARAN_BROKENISLES,		35.4,	29.8,	"Neutral")
+	AddQuest(40203,	Z.HIGHMOUNTAIN,			0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(40204,	Z.HIGHMOUNTAIN,			0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(40205,	Z.HIGHMOUNTAIN,			0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(40214,	Z.SURAMAR,			26.6,	71.6,	"Neutral")
+	AddQuest(41640,	Z.VALSHARAH,			54.6,	73.2,	"Neutral")
+	AddQuest(41642,	Z.STORMHEIM,			60.2,	51.2,	"Neutral")
+	AddQuest(41644,	Z.DALARAN_BROKENISLES,		33.4,	48.0,	"Neutral")
+	AddQuest(43943,	Z.SURAMAR,			0.0,	0.0,	"Neutral") --Needs updating
 
 	self.InitializeQuests = nil
 end
